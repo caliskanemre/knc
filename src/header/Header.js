c63509004a1384e4d4f2de74791b3be9d8fe1bb8
emre.caliskan <emre.caliskan@kuehne-nagel.com>
2024-01-13 16:27:13 +0200

    search mobile has changed

--- src/header/Header.js
+++ src/header/Header.js
@@ -8,7 +8,9 @@ import {NavLink} from 'react-router-dom';
 import {AccountCircle, PinDropOutlined} from "@mui/icons-material";
 import './Header.css';
 import SearchImage from "../search/search.png"
+import SearchMobileImage from "../search/searchMobile.png"
 
+const isMobile = window.innerWidth <= 600;
 function Header() {
 
     return (
@@ -16,21 +18,27 @@ function Header() {
             <Toolbar>
                 {/* Use NavLink for "activenty" to get the active styling */}
                 <NavLink to="/main" className="navbar-link navbar-brand" activeClassName="active">
-                    <Typography variant="h4" noWrap style={{ fontWeight: 'bold', fontSize: '1.5rem',color: 'darkorange'  }}>
-                        αctiventy
+                    <Typography variant="h4" noWrap style={{ fontWeight: 'bold', fontSize: '1.5rem', color: 'darkorange' }}>
+                        <span className="desktop-app-title">αctiventy</span>
+                    </Typography>
+                    <Typography variant="h1" noWrap style={{ fontWeight: 'bold', fontSize: '2.5rem', color: 'darkorange' }}>
+                        <span className="mobile-app-title">α</span>
                     </Typography>
                 </NavLink>
+                <NavLink to="/search" className="nav-link" activeClassName="active">
 
-                <NavLink
-                    to="/search"
-                    className="nav-link"
-                    activeClassName="active"
-                >
-                    <img
+                    <span className="search-desktop-image">
+                        <img
                         src={SearchImage}
                         alt="Search events"
-                        style={{ cursor: 'pointer', marginLeft: '40px', width: '380px' }} // This changes the cursor to a pointer when hovering over the image
-                    />
+                        style={{ cursor: 'pointer' }}/>
+                    </span>
+                    <span className="search-mobile-image">
+                        <img
+                        src={SearchMobileImage}
+                        alt="Search events"
+                        style={{ cursor: 'pointer', width: "100px", marginTop: "15px"}}/>
+                    </span>
                 </NavLink>
 
                 <NavLink
