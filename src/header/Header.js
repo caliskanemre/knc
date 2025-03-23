6017aba663499cb8a0175b4f8949da7a26036277
Emre Caliskan <emre.caliskan@kuehne-nagel.com>
2025-03-23 11:51:42 +0200

    username has removed

--- src/header/Header.js
+++ src/header/Header.js
@@ -208,6 +208,46 @@ export default function Header() {
 
                     <Box flexGrow={1}/>
 
+                    <IconButton
+                        aria-label="Instagram"
+                        href="https://www.instagram.com/knc_kina_ogranizasyon" // Kendi Instagram URL'nizi ekleyin
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        sx={{
+                            color: 'black',
+                            marginLeft: '10px',
+                            '&:hover': { color: '#8B0000' },
+                        }}
+                    >
+                        {/* Instagram SVG İkonu */}
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            style={{ marginRight: '10px' }} // İkon ve metin arasında boşluk
+                        >
+                            <defs>
+                                <linearGradient id="instaGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                                    <stop offset="0%" style={{ stopColor: '#feda75' }} />
+                                    <stop offset="20%" style={{ stopColor: '#fa7e1e' }} />
+                                    <stop offset="40%" style={{ stopColor: '#d62976' }} />
+                                    <stop offset="60%" style={{ stopColor: '#962fbf' }} />
+                                    <stop offset="100%" style={{ stopColor: '#4f5bd5' }} />
+                                </linearGradient>
+                            </defs>
+                            <path
+                                fill="url(#instaGradient)"
+                                d="M12 2.163c3.204 0 3.584.012 4.85.07 1.366.062 2.633.326 3.608 1.301.975.975 1.24 2.242 1.301 3.608.058 1.266.07 1.646.07 4.85s-.012 3.584-.07 4.85c-.062 1.366-.326 2.633-1.301 3.608-.975.975-2.242 1.24-3.608 1.301-1.266.058-1.646.07-4.85.07s-3.584-.012-4.85-.07c-1.366-.062-2.633-.326-3.608-1.301-.975-.975-1.24-2.242-1.301-3.608-.058-1.266-.07-1.646-.07-4.85s.012-3.584.07-4.85c.062-1.366.326-2.633 1.301-3.608.975-.975 2.242-1.24 3.608-1.301 1.266-.058 1.646-.07 4.85-.07zm0-2.163c-3.259 0-3.67.014-4.947.072-1.277.058-2.153.28-2.92.599-.79.33-1.454.794-2.118 1.458-.664.664-1.128 1.328-1.458 2.118-.319.767-.541 1.643-.599 2.92-.058 1.277-.072 1.688-.072 4.947s.014 3.67.072 4.947c.058 1.277.28 2.153.599 2.92.33.79.794 1.454 1.458 2.118.664.664 1.328 1.128 2.118 1.458.767.319 1.643.541 2.92.599 1.277.058 1.688.072 4.947.072s3.67-.014 4.947-.072c1.277-.058 2.153-.28 2.92-.599.79-.33 1.454-.794 2.118-1.458.664-.664 1.128-1.328 1.458-2.118.319-.767.541-1.643.599-2.92.058-1.277.072-1.688.072-4.947s-.014-3.67-.072-4.947c-.058-1.277-.28-2.153-.599-2.92-.33-.79-.794-1.454-1.458-2.118-.664-.664-1.328-1.128-2.118-1.458-.767-.319-1.643-.541-2.92-.599-1.277-.058-1.688-.072-4.947-.072z"
+                            />
+                            <path
+                                fill="url(#instaGradient)"
+                                d="M12 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.162 6.162 6.162 6.162-2.759 6.162-6.162-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"
+                            />
+                            <circle fill="url(#instaGradient)" cx="18.406" cy="5.594" r="1.44" />
+                        </svg>
+                    </IconButton>
+
                     {/* Cart Icon */}
                     <IconButton
                         aria-label="cart"
@@ -220,6 +260,8 @@ export default function Header() {
                     >
                         <ShoppingBagOutlinedIcon sx={{fontSize: 30}}/>
                     </IconButton>
+
+
 
                     {/* Login / User Menu */}
                     {!isLoggedIn ? (
@@ -293,6 +335,47 @@ export default function Header() {
                             >
                                 Kınasepeti
                             </Typography>
+                            <ListItem
+                                button
+                                component="a"
+                                href="https://www.instagram.com/knc_kina_organizasyon" // Kendi Instagram URL'nizi ekleyin
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <Box display="flex" alignItems="center">
+                                    {/* Renkli Instagram SVG İkonu */}
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        style={{ marginRight: '10px' }} // İkon ve metin arasında boşluk
+                                    >
+                                        <defs>
+                                            <linearGradient id="instaGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                                                <stop offset="0%" style={{ stopColor: '#feda75' }} />
+                                                <stop offset="20%" style={{ stopColor: '#fa7e1e' }} />
+                                                <stop offset="40%" style={{ stopColor: '#d62976' }} />
+                                                <stop offset="60%" style={{ stopColor: '#962fbf' }} />
+                                                <stop offset="100%" style={{ stopColor: '#4f5bd5' }} />
+                                            </linearGradient>
+                                        </defs>
+                                        <path
+                                            fill="url(#instaGradient)"
+                                            d="M12 2.163c3.204 0 3.584.012 4.85.07 1.366.062 2.633.326 3.608 1.301.975.975 1.24 2.242 1.301 3.608.058 1.266.07 1.646.07 4.85s-.012 3.584-.07 4.85c-.062 1.366-.326 2.633-1.301 3.608-.975.975-2.242 1.24-3.608 1.301-1.266.058-1.646.07-4.85.07s-3.584-.012-4.85-.07c-1.366-.062-2.633-.326-3.608-1.301-.975-.975-1.24-2.242-1.301-3.608-.058-1.266-.07-1.646-.07-4.85s.012-3.584.07-4.85c.062-1.366.326-2.633 1.301-3.608.975-.975 2.242-1.24 3.608-1.301 1.266-.058 1.646-.07 4.85-.07zm0-2.163c-3.259 0-3.67.014-4.947.072-1.277.058-2.153.28-2.92.599-.79.33-1.454.794-2.118 1.458-.664.664-1.128 1.328-1.458 2.118-.319.767-.541 1.643-.599 2.92-.058 1.277-.072 1.688-.072 4.947s.014 3.67.072 4.947c.058 1.277.28 2.153.599 2.92.33.79.794 1.454 1.458 2.118.664.664 1.328 1.128 2.118 1.458.767.319 1.643.541 2.92.599 1.277.058 1.688.072 4.947.072s3.67-.014 4.947-.072c1.277-.058 2.153-.28 2.92-.599.79-.33 1.454-.794 2.118-1.458.664-.664 1.128-1.328 1.458-2.118.319-.767.541-1.643.599-2.92.058-1.277.072-1.688.072-4.947s-.014-3.67-.072-4.947c-.058-1.277-.28-2.153-.599-2.92-.33-.79-.794-1.454-1.458-2.118-.664-.664-1.328-1.128-2.118-1.458-.767-.319-1.643-.541-2.92-.599-1.277-.058-1.688-.072-4.947-.072z"
+                                        />
+                                        <path
+                                            fill="url(#instaGradient)"
+                                            d="M12 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.162 6.162 6.162 6.162-2.759 6.162-6.162-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"
+                                        />
+                                        <circle fill="url(#instaGradient)" cx="18.406" cy="5.594" r="1.44" />
+                                    </svg>
+                                    <ListItemText
+                                        primary="Instagram"
+                                        primaryTypographyProps={{ style: { fontSize: '1.2rem' } }}
+                                    />
+                                </Box>
+                            </ListItem>
 
                             <ListItem button onClick={() => handleNavigation('/search')}>
                                 <Box display="flex" alignItems="center">
