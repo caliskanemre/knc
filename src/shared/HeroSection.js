d3e7747b147f83d9df1ca37be0f106e9634373a8
Emre Caliskan <emre.caliskan@kuehne-nagel.com>
2025-06-07 20:41:46 +0300

    HeroSection.js has changed

--- src/shared/HeroSection.js
+++ src/shared/HeroSection.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Box, Typography, Button, useTheme, useMediaQuery } from '@mui/material';
 // YENİ: Örnek video ve poster (kendi dosyalarınızla değiştirin)
-import videoPoster from './../images/kapak2.png'// Video yüklenemezse görünecek resim
+import videoPoster from './../images/IMG_6719.JPG'// Video yüklenemezse görünecek resim
 
 export default function HeroSection() {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
     // S3'teki videonun URL'si (kendi bucket URL'nizle değiştirin)
-    const heroVideo = 'https://kinasepeti.s3.eu-north-1.amazonaws.com/kina-video-2.mp4';
+    const heroVideo = 'https://kinasepeti.s3.eu-north-1.amazonaws.com/kina-video-3.mp4';
 
     return (
         <Box sx={{ position: 'relative', height: { xs: '30vh', md: '50vh' }, width: '100%', overflow: 'hidden' }}>
