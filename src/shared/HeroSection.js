f0ed09ff903200d7fe6a59b266ec5e347ed5c462
Emre Caliskan <emre.caliskan@kuehne-nagel.com>
2025-09-26 21:41:26 +0300

    next js

--- src/shared/HeroSection.js
+++ src/shared/HeroSection.js
@@ -10,7 +10,7 @@ export default function HeroSection({ isMobile, heroTitle, heroSubtitle, mobileI
 
     // Resimlerin hem webp hem de jpg versiyonlarının olduğunu varsayıyoruz.
     const mobileImageBaseUrl = "https://d2830psw11bu27.cloudfront.net/sade";
-    const desktopImageBaseUrl = "https://d2830psw11bu27.cloudfront.net/sade";
+    const desktopImageBaseUrl = "https://d2830psw11bu27.cloudfront.net/small_sade";
 
 
     return (
